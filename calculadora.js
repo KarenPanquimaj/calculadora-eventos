521a229fb8b9f776b618805c3564e495acd411fa
karen panquimaj <karenpanquimaj@gmail.com>
2024-05-28 17:48:32 -0600

    version 1

---
+++ calculadora.js
@@ -0,0 +1,80 @@
+
+
+const uno = document.querySelector('#uno')
+const dos = document.querySelector('#dos')
+const tres = document.querySelector('#tres')
+const clear = document.querySelector('#clear')
+const cuatro = document.querySelector('#cuatro')
+const cinco = document.querySelector('#cinco')
+const seis = document.querySelector('#seis')
+const menos = document.querySelector('#menos')
+const siete = document.querySelector('#siete')
+const ocho = document.querySelector('#ocho')
+const nueve = document.querySelector('#nueve')
+const suma = document.querySelector('#suma')
+const divison = document.querySelector('#divison')
+const cero = document.querySelector('#cero')
+const multiplidor = document.querySelector('#multiplidor')
+
+
+const calcular = document.querySelector('#calcular')
+
+uno.addEventListener('click', () => {
+    calcular.value = '1'
+})
+
+
+dos.addEventListener('click', () => {
+    calcular.value = '2'
+})
+
+tres.addEventListener('click', () => {
+    calcular.value = '3'
+})
+
+cuatro.addEventListener('click', () => {
+    calcular.value = '4'
+})
+
+cinco.addEventListener('click', () => {
+    calcular.value = '5'
+})
+
+seis.addEventListener('click', () => {
+    calcular.value = '6'
+})
+
+siete.addEventListener('click', () => {
+    calcular.value = '7'
+})
+
+ocho.addEventListener('click', () => {
+    calcular.value = '8'
+})
+
+nueve.addEventListener('click', () => {
+    calcular.value = '9'
+})
+
+cero.addEventListener('click', () => {
+    calcular.value = '0'
+})
+
+menos.addEventListener('click', () => {
+    calcular.value = '-'
+})
+
+suma.addEventListener('click', () => {
+    calcular.value = '+'
+})
+
+divison.addEventListener('click', () => {
+    calcular.value = '÷'
+})
+
+
+multiplidor.addEventListener('click', () => {
+    calcular.value = '*'
+})
+
+
